7b0251ed828f5d8fdbbe30b1955f1a118dd94579
Adelanwa Oreofe <emmanueldamilare121@gmail.com>
2025-10-27 01:29:53 +0100

    v1.0.1

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,9 @@
 from app import app
 from waitress import serve
+from services.postService import start_scheduler
+
+with app.app_context():
+    start_scheduler(app)
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8000)
