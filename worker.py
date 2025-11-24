c6fbe16b88ad31da2cebe5a6c03c493d348a9ac6
Adelanwa Oreofe <emmanueldamilare121@gmail.com>
2025-11-25 00:10:08 +0100

    scheduler updated

--- worker.py
+++ worker.py
@@ -1,12 +1,4 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+
 from services.postService import post_scheduled_posts
 
-scheduler = BlockingScheduler()
-
-@scheduler.scheduled_job("interval", seconds=60)
-def run_task():
-    post_scheduled_posts
-    print("Scheduled job executed")
-
-if __name__ == "__main__":
-    scheduler.start()
+post_scheduled_posts()
